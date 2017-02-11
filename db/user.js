11d7e871d9f898b5009f323dc184762dd97b67e4
Andrew <giangrant18@gmail.com>
2017-02-11 16:20:58 -0500

    added registration validation

--- db/user.js
+++ db/user.js
@@ -2,6 +2,7 @@ var mongodb = require('mongodb');
 var mongoClient = mongodb.MongoClient;
 var globals = require('../globals.js');
 var bcrypt = require('bcryptjs');
+var _ = require('underscore');
 
 exports.findById = function(id, callback) {
 	process.nextTick(function() {
@@ -62,8 +63,56 @@ exports.findByUsername = function(username, password, callback) {
 };
 
 exports.registerUser = function(req, res) {
+	if(!_.isObject(req.body)) {
+		return res.status(400).send({error: "Please fill out required fields."});
+	}
+	else {
+		var message = "";
+		if(!_.isString(req.body.username)) {
+			message += "Please enter an email address.\n";
+		}
+		else if(req.body.username.search(/^..*@..*\...*$/)) {
+			message += "Please enter a valid email address.\n";
+		}
+		if(!_.isString(req.body.password)) {
+			message += "Please enter a password.\n";
+		}
+		else {
+			if(req.body.password.search(/[A-Z]/) === -1) {
+				message += "Password must contain at least one uppercase character.\n";
+			}
+			if(req.body.password.search(/[a-z]/) === -1) {
+				message += "Password must contain at least one lowercase character.\n";
+			}
+			if(req.body.password.search(/[0-9]/) === -1) {
+				message += "Password must contain at least one number.\n";
+			}
+			if(req.body.password.length < 6) {
+				message += "Password must be more than 6 characters in length.\n";
+			}
+			if(req.body.password.length > 255){
+				message += "Password must be less than 255 characters in length.\n";
+			}
+			// fix this later
+			if(!_.isString(req.body.firstName)) {
+				message += "Please enter first name.\n";
+			}
+			if(!_.isString(req.body.lastName)) {
+				message += "Please enter last name.\n";
+			}
+			if(req.body.passwordConfirm !== req.body.password) {
+				message += "Confirm password and password do not match.\n";
+			}
+			if(message.length > 0) {
+				return res.status(400).send({error: message});
+			}
+			register(req, res);
+		}
+	}
+};
+
+function register(req, res) {
 	process.nextTick(function() {
-		console.log('trying to create user');
 		mongoClient.connect(globals.dbUrl, function(err, db) {
 			db.open();
 			if(err) {
@@ -83,7 +132,8 @@ exports.registerUser = function(req, res) {
 				if(!doc) {
 					var passwordSalt = bcrypt.genSaltSync(11);
 					var password = bcrypt.hashSync(req.body.password, passwordSalt);
-					collection.insertOne({username: req.body.username, password: password}, function(err) {
+					collection.insertOne({username: req.body.username, password: password,
+						firstName: req.body.firstName, lastName: req.body.lastName}, function(err) {
 						if(err) {
 							console.log(err.toString() + "blah2");
 							db.close();
@@ -93,8 +143,12 @@ exports.registerUser = function(req, res) {
 						res.redirect('/registerSuccess');
 					});
 				}
+				else {
+					db.close();
+					return res.status(400).send({error: "Email address is already being used."});
+				}
 				res.redirect('/login');
 			});
 		});
 	});
-};
+}
